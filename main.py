e8083f7a5cdec996546c27dcaa172146e92106fe
Justin Woodmansee <justinwoodmansee@Justins-MacBook-Pro.local>
2020-09-01 14:24:15 -0600

    removed commented code

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-# chromeDriver = webdriver.Chrome()
-# # foxDriver = webdriver.Firefox()
-
 
 def loginTest(browser):
     if browser == "chrome":
@@ -24,16 +21,6 @@ def loginTest(browser):
     driver.quit()
 
 
-# # def loginTest_FireFox():
-# #     foxDriver.get("https://jwoodmansee.printercloud.com/admin/")
-# #     print(foxDriver.current_url)
-# #     # foxDriver.find_element_by_id("relogin_user").send_keys("username")
-# #     # foxDriver.find_element_by_id("relogin_password").send_keys("password")
-# #     # foxDriver.find_element_by_id("admin-login-btn").send_keys(Keys.ENTER)
-# #     # print(foxDriver.title)
-# #     # time.sleep(30)
-# #     foxDriver.close()
-
 def testForgotPassword(browser):
     if browser == "chrome":
         driver = webdriver.Chrome()
@@ -47,17 +34,12 @@ def testForgotPassword(browser):
     print(driver.current_url)
 
 
-# loginTest_Chrome()
-# # loginTest_FireFox()
-
 def run():
     browsers = ["chrome", "firefox", "safari"]
     print(browsers)
     for b in range(len(browsers)):
-    #     # print(drivers[d])
-    #     loginTest(browsers[b])
+        loginTest(browsers[b])
         testForgotPassword(browsers[b])
-    
 
 
 if __name__ == "__main__":
